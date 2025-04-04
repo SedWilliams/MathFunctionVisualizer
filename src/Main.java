bb16b721564d4c7b351e4c5bfa6c40c04fcf9309
tre3iii <lilshed3@gmail.com>
2025-04-04 14:00:20 -0700

    functional graph

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,35 @@ import processing.core.PApplet;
 
 /*
 TODO:
--return origin to start function from, from the graph object
+-return origin to start function from, from the mainGraph object
 */
 
 public class Main extends PApplet {
 
-    float x = 0;
-    float y = height;
+    graph mainGraph;
+    float x;
+    float y;
 
     public void settings() {
         size(1400, 800);
+
     }
 
    public void setup() {
         background(0);
 
-        drawFunction();
+        //drawFunction();
         frameRate(200);
 
-       graph graph = new graph(this, 500f, 500f, 10f, 10f, 50f, 50f);
 
+        scale(1.30f);
+        translate(100, height-400);
+        mainGraph = new graph(this, 500f, 500f, 50f, 50f, 0f, 0f - 500f);
+        mainGraph.drawGraph();
+        x = mainGraph.graphLocX;
+        y = height;
+        scale(1f);
+        translate(-100, -1*(height-400));
    }
 
     public void draw() {
@@ -40,14 +49,7 @@ public class Main extends PApplet {
         translate(-100, -1*(height-400));
     }
 
-    public void drawFunction() {
-        x+=0.1f;
-        y = -(x*x);
-
-        ellipse(x, y, 10, 10);
-    }
-
     public static void main(String[] args) {
-        PApplet.main("Main", args);
+        PApplet.main("Main");
     }
 }
--- src/graph.java
+++ src/graph.java
@@ -1,14 +1,14 @@
-import processing.core.*;
+import processing.core.PApplet;
 
-public class graph extends PApplet{
+public class graph {
 
     PApplet p;
-    private float graphWidth;
-    private float graphHeight;
-    private float graphIntervalX;
-    private float graphIntervalY;
-    private float graphLocX;
-    private float graphLocY;
+    float graphWidth;
+    float graphHeight;
+    float graphIntervalX;
+    float graphIntervalY;
+    float graphLocX;
+    float graphLocY;
 
     public graph(PApplet p, float graphWidth, float graphHeight, float graphIntervalX, float graphIntervalY, float graphLocX, float graphLocY) {
         this.p = p;
@@ -18,27 +18,23 @@ public class graph extends PApplet{
         this.graphIntervalY = graphIntervalY;
         this.graphLocX = graphLocX;
         this.graphLocY = graphLocY;
-
-        drawGraph();
     }
 
     void drawGraph() {
         p.stroke(255);
-        p.fill(0);
+        p.fill(0, 0);
 
         p.rect(this.graphLocX, this.graphLocY, this.graphWidth, this.graphHeight);
 
         //draw y lines
         for(float i = this.graphLocX + this.graphIntervalX; i < this.graphLocX + this.graphWidth; i+=this.graphIntervalX) {
-            p.noStroke();
-            p.fill(190, 190);
+            p.fill(190, 255);
             p.line(i, this.graphLocY, i, this.graphLocY+this.graphHeight);
         }
 
         //draw x lines
         for(float i = this.graphLocY + this.graphIntervalY; i < this.graphLocY + this.graphHeight; i+=this.graphIntervalY) {
-            p.noStroke();
-            p.fill(190, 190);
+            p.fill(190, 255);
             p.line(this.graphLocX, i, this.graphLocX+this.graphWidth, i);
         }
     }
